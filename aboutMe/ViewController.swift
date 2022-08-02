1c78ecea854a9d956c3776ba5dc247ee553b81c3
Talia Barsness <blue.sky737@icloud.com>
2022-08-02 13:23:22 -0400

    Initial Commit

---
+++ aboutMe/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  aboutMe
+//
+//  Created by Talia Barsness on 7/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func factButton(_ sender: Any) {
+        clickButton.titleLabel!.text = "I love reading and going to the beach!"
+    }
+    
+}
+
